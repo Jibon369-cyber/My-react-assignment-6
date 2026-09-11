b06feb83b4267bc152b74b36d192038334060723
Jibon369-cyber <ahjibon466@gmail.com>
2026-09-11 16:36:18 +0600

    my first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+
+
+import Navbar from './Components/Layout/Navbar'
+
+function App() {
+  
+
+  return (
+    <>
+      <Navbar />
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/Layout/Navbar.tsx
@@ -0,0 +1,52 @@
+import logoText from "../../assets/logo-text.png";
+
+export default function Navbar() {
+  return (
+    <header className='sticky top-0 z-50 border-b border-gray-100 bg-white'>
+      <nav className='container mx-auto flex max-w-6xl items-center justify-between px-4 py-4'>
+        <div>
+          <img src={logoText} alt='Logo' />
+        </div>
+
+        <div className='hidden items-center gap-6 md:flex'>
+          <a href='#' className='text-xs font-bold text-brand-pink'>
+            Home
+          </a>
+
+          <a
+            href='#'
+            className='text-xs font-bold text-gray-600 hover:text-brand-pink'>
+            Technologies
+          </a>
+
+          <a
+            href='#'
+            className='text-xs font-bold text-gray-600 hover:text-brand-pink'>
+            Projects
+          </a>
+
+          <a
+            href='#'
+            className='text-xs font-bold text-gray-600 hover:text-brand-pink'>
+            About
+          </a>
+
+          <a
+            href='#'
+            className='text-xs font-bold text-gray-600 hover:text-brand-pink'>
+            Contact
+          </a>
+        </div>
+
+        <div className='flex items-center gap-3'>
+          <button className='hidden text-xs font-medium text-gray-600 sm:block'>
+            Sign In
+          </button>
+          <button className='rounded-full bg-pink-700 px-4 py-2 text-xs font-medium text-white'>
+            Sign Up
+          </button>
+        </div>
+      </nav>
+    </header>
+  );
+}
