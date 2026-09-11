4aa174452527e185a5f27260ca3793a4e4a1f2ed
Jibon369-cyber <ahjibon466@gmail.com>
2026-09-11 18:53:20 +0600

    Add category section

--- src/Types/technology.ts
+++ src/Types/technology.ts
@@ -1,10 +1,25 @@
+export type TechnologyCategory =
+  | "Frontend"
+  | "Backend"
+  | "Database"
+  | "Language"
+  | "Styling"
+  | "DevOps"
+  | "Tools";
+
+export type Difficulty =
+  | "Beginner-Friendly"
+  | "Intermediate"
+  | "Advanced";
+
+
 export interface ITechnology {
-    id: string,
-    name: string,
-    category: string,
-    description: string,
-    icon: string,
-    rating: number,
-    difficulty: string,
-    badge: string
+  id: string;
+  name: string;
+  category: TechnologyCategory;
+  description: string;
+  icon: string;
+  rating: number;
+  difficulty: string;
+  badge: string;
 }
